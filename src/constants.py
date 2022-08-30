a06af7ff8628445c4193e129caa5f1f74652cf16
Gabriel García Fernández <g.gar.fern@hotmail.com>
2022-08-31 00:17:49 +0200

    check pylint

--- src/constants.py
+++ src/constants.py
@@ -1,25 +1,26 @@
 from enum import Enum
 
 class BaseEnum(Enum):
-	def __ge__(self, other):
-		if self.__class__ is other.__class__:
-			return self.value >= other.value
-		return NotImplemented
-	def __gt__(self, other):
-		if self.__class__ is other.__class__:
-			return self.value > other.value
-		return NotImplemented
-	def __le__(self, other):
-		if self.__class__ is other.__class__:
-			return self.value <= other.value
-		return NotImplemented
-	def __lt__(self, other):
-		if self.__class__ is other.__class__:
-			return self.value < other.value
-		return NotImplemented
+    def __ge__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value >= other.value
+        return NotImplemented
+    def __gt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value > other.value
+        return NotImplemented
+    def __le__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value <= other.value
+        return NotImplemented
+    def __lt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value < other.value
+        return NotImplemented
 
 class State(BaseEnum):
-	CREATED = 0
-	RUNNING = 1
-	PAUSED  = 2
-	STOPPED = 3
+    CREATED = 0
+    RUNNING = 1
+    PAUSED  = 2
+    STOPPED = 3
+    
--- test/util.py
+++ test/util.py
@@ -2,21 +2,24 @@ import string
 
 # FROM https://stackoverflow.com/a/20250018/13758294
 class PartialFormatter(string.Formatter):
-	def __init__(self, missing='[NOT FOUND]', bad_fmt='[BAD FORMATTING]'):
-		self.missing, self.bad_fmt=missing, bad_fmt
+    def __init__(self, missing='[NOT FOUND]', bad_fmt='[BAD FORMATTING]'):
+        self.missing, self.bad_fmt=missing, bad_fmt
 
-	def get_field(self, field_name, args, kwargs):
-		try:
-			val = super().get_field(field_name, args, kwargs)
-		except (KeyError, AttributeError):
-			val = None, field_name 
-		return val
+    def get_field(self, field_name, args, kwargs):
+        try:
+            val = super().get_field(field_name, args, kwargs)
+        except (KeyError, AttributeError):
+            val = None, field_name 
+        return val
 
-	def format_field(self, value, spec):
-		# handle an invalid format
-		if value==None: return self.missing
-		try:
-			return super().format_field(value, spec)
-		except ValueError:
-			if self.bad_fmt is not None: return self.bad_fmt   
-			else: raise
+    def format_field(self, value, format_spec):
+        # handle an invalid format
+        if value is None:
+            return self.missing
+        try:
+            return super().format_field(value, format_spec)
+        except ValueError:
+            if self.bad_fmt is not None:
+                return self.bad_fmt
+            else: raise
+            
