7ef64cc4e09080bef9a7f87aa94d945dfe7523ee
g-gar <g.gar.fern@hotmail.com>
2022-08-31 23:30:42 +0200

    create SynchronousExecutionFlow (#19)
    * create SynchronousExecutionFlow
    
    * update github workflows

--- src/definitions.py
+++ src/definitions.py
@@ -1,32 +1,30 @@
 
+from src.execution_flows import ExecutionFlow
+from src.processors import Processor
+
+
 class Loop:
     '''Main class
     '''
-
     def __init__(self):
         self.__index_generator__ = None
-        self.__processors__ = {}
+        self.__processors__ = dict[int, list[Processor]]()
 
     def index_generator(self, generator):
         '''Builder method for setting a generator function
         '''
-
         self.__index_generator__ = generator
         return self
 
     def add_processor(self, processor, priority = 0):
         '''Builder method for setting a processor with a priority
         '''
-
         self.__processors__[priority] = [*self.__processors__.get(priority, []), *[processor]]
         return self
 
-    def run(self):
+    def run(self, execution_flow:ExecutionFlow):
         '''Entry point for running the execution flow assigned in Loop.execution_flow()
         '''
-
-        for index in self.__index_generator__:
-            for processor in self.__processors__.get(0, None):
-                processor.run(index=index)
+        execution_flow.run(self.__index_generator__, self.__processors__)
         self.__index_generator__.close()
         self.__index_generator__.reset()
--- src/execution_flows.py
+++ src/execution_flows.py
@@ -1,28 +1,18 @@
 from abc import ABCMeta, abstractmethod
-from typing import Dict, List
 
 from src.processors import Processor
-from src.util import not_none
 
 class ExecutionFlow(metaclass=ABCMeta):
     '''Metaclass for execution flows'''
 
-    def __init__(self, generator = None, processors : Dict[int, List[Processor]] = None):
-        self.__generator__ = generator
-        self.__processors__ = processors
-
-    @not_none
-    def generator(self, generator) -> any:
-        '''Generator'''
-        self.__generator__ = generator
-        return self
-
-    @not_none
-    def processors(self, **kwargs : Dict[int, List[Processor]]) -> any:
-        '''Processors'''
-        self.__processors__ = kwargs
-        return self
-
     @abstractmethod
-    def run(self):
+    def run(self, generator, processors : dict[int, list[Processor]]):
         '''Run method'''
+
+class SynchronousExecutionFlow(ExecutionFlow):
+    '''Synchronous execution flow'''
+
+    def run(self, generator, processors : dict[int, list[Processor]]):
+        for idx in generator:
+            for processor in processors.get(0, None):
+                processor.run(index=idx)
